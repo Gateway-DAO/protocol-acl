f6f577c19b4675d6c1f93f71a046718efcb76a0f
root <root@inst-builder-debian-12-build-build-zz6hj.us-central1-a.c.gce-image-builder.internal>
2024-07-19 16:26:00 +0000

    Added logics to solana acl

--- programs/sol-gateway/src/instructions/update_file.rs
+++ programs/sol-gateway/src/instructions/update_file.rs
@@ -12,12 +12,12 @@ pub struct UpdateFile<'info> {
         bump = file.bump,
         constraint = file.authority == signer.key() || (file.recovery.is_some() && file.recovery.unwrap() == signer.key())   @ Errors::UnauthorizedAuthorityUpdate,
     )]
-    pub app: Account<'info, File>,
+    pub file: Box<Account<'info, File>>,
     pub system_program: Program<'info, System>,
 }
 
 pub fn update_file(ctx: Context<UpdateFile>, file_data: UpdateFileData) -> Result<()> {
-    let app = &mut ctx.accounts.app;
+    let app = &mut ctx.accounts.file;
     app.authority = file_data.authority;
     app.recovery = file_data.recovery;
     app.name = validate_string_len(&file_data.name, 0, 16)?;
@@ -29,8 +29,8 @@ pub fn update_file(ctx: Context<UpdateFile>, file_data: UpdateFileData) -> Resul
         program_authority_field(&file_data.authority, app.expires_at, file_data.expires_at)?;
     emit!(FileChanged {
         time: utc_now(),
-        file_id: ctx.accounts.app.id,
-        authority: ctx.accounts.app.authority,
+        file_id: ctx.accounts.file.id,
+        authority: ctx.accounts.file.authority,
     });
     Ok(())
 }
