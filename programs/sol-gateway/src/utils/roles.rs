9ca0180b1808c84c32ed6148c8be1625ca2d15a5
Nuno Carvalho <nunoormaker21@gmail.com>
2024-09-05 17:42:26 +0100

    feat: created helper function to check roles

--- programs/sol-gateway/src/utils/roles.rs
+++ programs/sol-gateway/src/utils/roles.rs
@@ -1,3 +1,4 @@
+use crate::state::role::RoleType;
 use anchor_lang::prelude::*;
 
 pub fn address_or_wildcard(address: &Option<Pubkey>) -> &[u8] {
@@ -5,4 +6,13 @@ pub fn address_or_wildcard(address: &Option<Pubkey>) -> &[u8] {
         return b"*".as_ref();
     }
     address.as_ref().unwrap().as_ref()
+}
+
+pub fn allowed_roles(roles: &Vec<RoleType>, allowed_roles: &Vec<RoleType>) -> bool {
+    for role in roles {
+        if allowed_roles.contains(role) {
+            return true;
+        }
+    }
+    false
 }
